c00606556b235fead39986cc0367e991407d4b77
andrew <andrew@oddsandmore.com>
2021-02-23 10:02:23 +0100

    Add project files.

---
+++ Secure_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Secure_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
